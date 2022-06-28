2cce305e720aa8a705d29b939ba3e1e43c3d90d0
dyahputrin <dyah.nariswari@tokopedia.com>
2022-06-28 15:52:22 +0700

    base project

---
+++ What To Eat/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  What To Eat
+//
+//  Created by dyah.nariswari on 28/06/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    let ballArray = [#imageLiteral(resourceName: "ball1.png"),#imageLiteral(resourceName: "ball2.png"),#imageLiteral(resourceName: "ball3.png"),#imageLiteral(resourceName: "ball4.png"),#imageLiteral(resourceName: "ball5.png")]
+
+
+
+}
+
